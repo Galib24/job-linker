2ca53d69820119721ae43c27b3b585f9936f4c57
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-08-09 12:33:31 +0600

    add demo footer by shakil

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from '@/components/Banner/Banner';
+import Footer from '@/components/Footer/Footer';
 import React from 'react';
 
 const Home = () => {
-  return  <div className='text-7xl text-blue-500'>
+  return <div className='text-7xl text-blue-500'>
     Home
     <Banner></Banner>
-    </div>;
+    <Footer />
+  </div>;
 };
 
 export default Home;
