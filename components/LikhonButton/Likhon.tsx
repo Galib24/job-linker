fab55e13f1f453e0ba6a9549a57d22798840591f
IamLikhon1 <likhonadnan55@gmail.com>
2023-08-10 23:38:12 +0600

    from likhon

---
+++ components/LikhonButton/Likhon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Likhon = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center mt-10 font-bold'>This is From Likhon</h1>
+            
+        </div>
+    );
+};
+
+export default Likhon;
