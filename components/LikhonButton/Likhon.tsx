e56aba1615ad5826b1bab7f5214f7507e86387f8
IamLikhon1 <likhonadnan55@gmail.com>
2023-08-10 23:45:31 +0600

    from likhon button

--- components/LikhonButton/Likhon.tsx
+++ components/LikhonButton/Likhon.tsx
@@ -4,6 +4,8 @@ const Likhon = () => {
     return (
         <div>
             <h1 className='text-3xl text-center mt-10 font-bold'>This is From Likhon</h1>
+
+            <button className='px-3 py-2 bg-red-400 text-lg rounded-xl md:ml-96 text-white'>click me</button>
             
         </div>
     );
